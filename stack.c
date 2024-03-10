175587060428a4547dc38d561a75a5b708dcd0cb
Baptiste Dromer <baptiste.dromer@gmail.com>
2024-03-10 12:01:11 +0100

    chore: format with clang-format

--- stack.c
+++ stack.c
@@ -2,24 +2,22 @@
 
 #include <stdio.h>
 
-void f(int*);
+void f(int *);
 
-int main()
-{
-    int x = 1;
-    f(&x);
+int main() {
+  int x = 1;
+  f(&x);
 }
 
-void f(int *x)
-{
-    int y = 1;
-    
-    printf("address of x: %p\n", x);
-    printf("address of y: %p\n", &y);
+void f(int *x) {
+  int y = 1;
 
-    if (x > &y) {
-        printf("The stack grows down.\n");
-    } else {
-        printf("The stack grows up.\n");
-    }
+  printf("address of x: %p\n", x);
+  printf("address of y: %p\n", &y);
+
+  if (x > &y) {
+    printf("The stack grows down.\n");
+  } else {
+    printf("The stack grows up.\n");
+  }
 }
